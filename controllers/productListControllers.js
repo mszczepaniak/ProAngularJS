6c40af4910c721b3e4bcd5329a56cfcd6b37a72c
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2014-11-29 19:14:16 +0100

    Add the active category selection, now when user clicks the category, the css class is changed (using bootstrap)

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -2,13 +2,18 @@
  * Created by Michal on 25/11/2014.
  */
 angular.module("sportsStore")
-    .controller("productListCtrl", function($scope, $filter){
+    // product list active is used when category is active
+    .constant("productListActiveClass", "btn-primary")
+    .controller("productListCtrl", function($scope, $filter, productListActiveClass) {
     var selectedCategory = null;
-    $scope.selectedCategory = function(newCategory){
+    $scope.selectCategory = function(newCategory){
         selectedCategory = newCategory;
     };
     $scope.categoryFilterFn = function(product) {
         return selectedCategory == null || product.category == selectedCategory;
-        }
+    };
+    $scope.getCategoryClass = function (category){
+        return selectedCategory == category ? productListActiveClass : "";
+    };
     });
 

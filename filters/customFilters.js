e3d33b0b143796c635018f392cfabe5257d79acc
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2014-11-29 12:14:43 +0100

    Initial Commit, beginning of chapter 6

---
+++ filters/customFilters.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Michal on 24/11/2014.
+ */
+angular.module("customFilters", [])
+    .filter("unique",function(){
+        return function (data, propertyName){
+            if(angular.isArray(data) && angular.isString(propertyName)) {
+            var results = [];
+            var keys = {};
+            for(var i= 0; i< data.length; i++){
+                var val = data[i][propertyName];
+                if(angular.isUndefined(keys[val])){
+                keys.val=true;
+                results.push(val);
+                }
+            }
+            return results;
+            } else {
+                return data;
+            }
+        }
+    });
